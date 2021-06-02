8f7b6f35c77247158ac25a6583898cfd3e4889cb
Yusuf Ismail <yusufgamer222@gmail.com>
2021-06-02 10:27:59 +0100

    test

--- js/functions.js
+++ js/functions.js
@@ -2295,6 +2295,16 @@ darkModeToggle.addEventListener('click', () => {
             disableDarkMode();
         }
     })
+	})
+
+	$( ".inner-switch" ).on("click", function() {
+		if( $( "body" ).hasClass( "dark" )) {
+		  $( "body" ).removeClass( "dark" );
+		  $( ".inner-switch" ).text( "OFF" );
+		} else {
+		  $( "body" ).addClass( "dark" );
+		  $( ".inner-switch" ).text( "ON" );
+		}
 	});
 
 })(jQuery);
